47a865d0d2f33fe111728fe36077b70f2c90fb90
Luigi Calori <l.calori@cineca.it>
2012-01-20 17:20:47 +0000

    fixed activemq download

--- lib/BA/ActiveMQ-cpp/3.1.3.sh
+++ lib/BA/ActiveMQ-cpp/3.1.3.sh
@@ -4,7 +4,7 @@ ba_module_version=3.1.3
 #ba_req_modules="cmake"
 
 ba_home_url="http://activemq.apache.org/cms/index.html"
-ba_download_url="svn co https://svn.apache.org/repos/asf/activemq/activemq-cpp/tags/activemq-cpp-3.1.3/"
+ba_download_url="svn co https://svn.apache.org/repos/asf/activemq/activemq-cpp/tags/activemq-cpp-3.1.3/activemq-cpp"
 ba_short_desc="ActiveMQ-cpp library"
 ba_long_desc="cpp client library of Apache ActiveMQ message passing OLD version for ParaViewWeb"
 ba_license="unknown"
