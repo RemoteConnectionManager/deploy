b2946c8794f34cd07cfa4306251aeb145433069b
Luigi Calori <l.calori@cineca.it>
2012-01-20 17:20:45 +0000

    removed cmake module requirements

--- lib/BA/ParaView/paraviewweb.sh
+++ lib/BA/ParaView/paraviewweb.sh
@@ -3,7 +3,7 @@ ba_category=applications
 autopackage_assembly=ParaViewWeb
 #autopackage_config_args="-D${autopackage_name}_static:bool=ON"
 ba_module_version=3.10.1
-ba_req_modules="cmake ParaView/3.10.1_qt_shared"
+ba_req_modules="ParaView/3.10.1_qt_shared"
 
 ba_home_url="http://httpd.apache.org"
 ba_download_url="http://mirror.nohup.it/apache/httpd/httpd-2.2.17.tar.gz"
