7ab079b6d0d5d69b34abfe36130b72ef7383da88
Luigi Calori <l.calori@cineca.it>
2015-02-03 14:22:58 +0000

    first test of propro01 build

--- trunk/build/deploy/galileo.sh
+++ trunk/build/deploy/galileo.sh
@@ -7,11 +7,11 @@ python  ba_helper/ba_helper.py -a Packages/nasm/2.07
 python  ba_helper/ba_helper.py -a Packages/cmake/3.1.1/bin386.cfg
 python  ba_helper/ba_helper.py -a Packages/libjpeg-turbo/1.3.1
 python  ba_helper/ba_helper.py -a Packages/turbovnc/svn
-python  ba_helper/ba_helper.py -a --stop_on_error Packages/virtualgl/2.3.90
-python  ba_helper/ba_helper.py -a --stop_on_error Packages/rcm/1.2/
-python  ba_helper/ba_helper.py -a --stop_on_error Packages/cmake/3.1.1
+python  ba_helper/ba_helper.py -a Packages/virtualgl/2.3.90
+python  ba_helper/ba_helper.py -a Packages/rcm/1.2/
+#python  ba_helper/ba_helper.py -a --stop_on_error Packages/cmake/3.1.1
 #python ba_helper/ba_helper.py -a Packages/paraview/4.2.0/bin64bit.cfg
-#python ba_helper/ba_helper.py -a Packages/blender/2.72b/bin64bit.cfg
+python ba_helper/ba_helper.py -a Packages/blender/2.72b/bin64bit.cfg
 #python ba_helper/ba_helper.py -a --stop_on_error Packages/vapor/2.3.0/bin64bit.cfg
 #python ba_helper/ba_helper.py   -a  Packages/cmake/3.1.0-rc2/
 
