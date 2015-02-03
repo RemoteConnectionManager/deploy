01816f80c1206d1b40ee66cbffec3c4889789f76
Luigi Calori <l.calori@cineca.it>
2015-02-03 14:36:36 +0000

    galieo seems to have already a recent cmake

--- trunk/build/deploy/galileo.sh
+++ trunk/build/deploy/galileo.sh
@@ -4,7 +4,7 @@ set -o errexit
 module load autoload ba
 echo $PWD
 python  ba_helper/ba_helper.py -a Packages/nasm/2.07
-python  ba_helper/ba_helper.py -a Packages/cmake/3.1.1/bin386.cfg
+#python  ba_helper/ba_helper.py -a Packages/cmake/3.1.1/bin386.cfg
 python  ba_helper/ba_helper.py -a Packages/libjpeg-turbo/1.3.1
 python  ba_helper/ba_helper.py -a Packages/turbovnc/svn
 python  ba_helper/ba_helper.py -a Packages/virtualgl/2.3.90
