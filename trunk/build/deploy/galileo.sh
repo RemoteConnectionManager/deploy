dccf95fca223940f99378f03eb2c19199a432b16
Luigi Calori <l.calori@cineca.it>
2015-02-13 14:46:32 +0000

    adding module paraview/data

--- trunk/build/deploy/galileo.sh
+++ trunk/build/deploy/galileo.sh
@@ -9,6 +9,8 @@ python  ba_helper/ba_helper.py -a Packages/libjpeg-turbo/1.3.1
 python  ba_helper/ba_helper.py -a Packages/turbovnc/svn
 python  ba_helper/ba_helper.py -a Packages/virtualgl/2.3.90
 python  ba_helper/ba_helper.py -a Packages/rcm/1.2/
+python  ba_helper/ba_helper.py -a Packages/paraview/4.3.1/bin64bit.cfg
+python  ba_helper/ba_helper.py -a Packages/paraview/data
 #python  ba_helper/ba_helper.py -a --stop_on_error Packages/cmake/3.1.1
 #python ba_helper/ba_helper.py -a Packages/paraview/4.2.0/bin64bit.cfg
 python ba_helper/ba_helper.py -a Packages/blender/2.72b/bin64bit.cfg
